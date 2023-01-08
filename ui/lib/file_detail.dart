807a59b0ad619572a123e5eaf8077bfd7ca0eaff
Sherlock Holo <sherlockya@gmail.com>
2023-01-08 16:09:12 +0800

    refactor: split state into FileList and PeerList widget

--- ui/lib/file_detail.dart
+++ ui/lib/file_detail.dart
@@ -1,13 +1,149 @@
+import 'dart:convert';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 import 'package:mime/mime.dart';
+import 'package:ui/list_file_response.dart';
 
-class FileList extends StatelessWidget {
-  final List<FileDetail> files;
+class FileList extends StatefulWidget {
+  const FileList({super.key});
 
-  const FileList({super.key, required this.files});
+  @override
+  State<StatefulWidget> createState() => _FileListState();
+}
+
+class _FileListState extends State<FileList> {
+  Future<int> _uploadFile() async {
+    final result = await FilePicker.platform
+        .pickFiles(withData: false, withReadStream: true);
+    if (result == null) {
+      return 0;
+    }
+
+    Uri url;
+    if (Uri.base.scheme == "http") {
+      url = Uri.http(Uri.base.authority, "/api/upload_file");
+    } else {
+      url = Uri.https(Uri.base.authority, "/api/upload_file");
+    }
+
+    final file = result.files.single;
+    final mimeType = lookupMimeType(file.name);
+    final contentType = mimeType != null ? MediaType.parse(mimeType) : null;
+    final multipartFile = http.MultipartFile(
+        file.name, file.readStream!, file.size,
+        filename: file.name, contentType: contentType);
+
+    final request = http.MultipartRequest("POST", url)
+      ..files.add(multipartFile);
+
+    return (await request.send()).statusCode;
+  }
 
   @override
   Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Column(
+        children: [
+          ListTile(
+            title: const Text(
+              "File List",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      _uploadFile().then((value) {
+                        if (value == 0) {
+                          return;
+                        }
+
+                        if (value != 200) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(
+                            "Upload failed: $value",
+                            style: const TextStyle(color: Colors.red),
+                          )));
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text("Upload done")));
+                        }
+
+                        setState(() {});
+                      });
+                    },
+                    icon: const Icon(Icons.add)),
+                IconButton(
+                    onPressed: () {
+                      setState(() {});
+                    },
+                    icon: const Icon(Icons.refresh))
+              ],
+            ),
+          ),
+          Expanded(child: _createFileListWidget())
+        ],
+      ),
+    );
+  }
+
+  FutureBuilder<List<FileDetail>> _createFileListWidget() {
+    return FutureBuilder<List<FileDetail>>(
+      future: _getFileList(),
+      builder:
+          (BuildContext context, AsyncSnapshot<List<FileDetail>> snapshot) {
+        if (snapshot.hasData) {
+          return _buildList(snapshot.data!);
+        }
+
+        if (snapshot.hasError) {
+          return Text(
+            "${snapshot.error!}",
+            style: const TextStyle(color: Colors.red, fontSize: 20),
+          );
+        }
+
+        return const SizedBox(
+          width: 60,
+          height: 60,
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
+  Future<List<FileDetail>> _getFileList() async {
+    Uri url;
+    if (Uri.base.scheme == "http") {
+      url = Uri.http(Uri.base.authority, "/api/list_files");
+    } else {
+      url = Uri.https(Uri.base.authority, "/api/list_files");
+    }
+
+    final resp = await http.get(url);
+    if (resp.statusCode != 200) {
+      throw Exception("status code ${resp.statusCode} != 200");
+    }
+
+    final respBody = utf8.decode(resp.bodyBytes);
+    final json = jsonDecode(respBody) as Map<String, dynamic>;
+    final listResp = ListFileResponse.fromJson(json);
+
+    return listResp.files.map((file) {
+      return FileDetail(
+          filename: file.filename,
+          hash: file.hash,
+          size: file.size,
+          downloaded: file.downloaded);
+    }).toList();
+  }
+
+  Widget _buildList(List<FileDetail> files) {
     return ListView.builder(
       itemCount: files.length,
       itemBuilder: (context, index) {
@@ -72,9 +208,8 @@ class FileDetail {
   final String size;
   final bool downloaded;
 
-  const FileDetail(
-      {required this.filename,
-      required this.hash,
-      required this.size,
-      required this.downloaded});
+  const FileDetail({required this.filename,
+    required this.hash,
+    required this.size,
+    required this.downloaded});
 }
--- ui/lib/peer_info.dart
+++ ui/lib/peer_info.dart
@@ -1,17 +1,18 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:ui/list_peers_response.dart';
 
 class PeerInfo {
   final String peerId;
   final List<String> addrs;
-  bool expanded = false;
 
   PeerInfo(this.peerId, this.addrs);
 }
 
 class PeerList extends StatefulWidget {
-  final List<PeerInfo> peers;
-
-  const PeerList({super.key, required this.peers});
+  const PeerList({super.key});
 
   @override
   State<StatefulWidget> createState() => _PeerListState();
@@ -20,44 +21,108 @@ class PeerList extends StatefulWidget {
 class _PeerListState extends State<PeerList> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        child: _buildPanel(),
-      ),
+    return Expanded(
+        flex: 1,
+        child: Column(
+          children: [
+            ListTile(
+              title: const Text(
+                "Peer List",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.refresh)),
+                ],
+              ),
+            ),
+            Expanded(child: _createPeerListWidget())
+          ],
+        ));
+  }
+
+  Future<List<PeerInfo>> _getPeerList() async {
+    Uri url;
+    if (Uri.base.scheme == "http") {
+      url = Uri.http(Uri.base.authority, "/api/list_peers");
+    } else {
+      url = Uri.https(Uri.base.authority, "/api/list_peers");
+    }
+
+    final resp = await http.get(url);
+    if (resp.statusCode != 200) {
+      throw Exception("status code ${resp.statusCode} != 200");
+    }
+
+    final respBody = utf8.decode(resp.bodyBytes);
+    final json = jsonDecode(respBody) as Map<String, dynamic>;
+    final listResp = ListPeersResponse.fromJson(json);
+
+    return listResp.peers
+        .map((peer) => PeerInfo(peer.peer, peer.connectedAddrs))
+        .toList();
+  }
+
+  FutureBuilder<List<PeerInfo>> _createPeerListWidget() {
+    return FutureBuilder<List<PeerInfo>>(
+      future: _getPeerList(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return _buildPeerList(snapshot.data!);
+        }
+
+        if (snapshot.hasError) {
+          return Text(
+            "${snapshot.error!}",
+            style: const TextStyle(color: Colors.red, fontSize: 20),
+          );
+        }
+
+        return const SizedBox(
+          width: 60,
+          height: 60,
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 
-  Widget _buildPanel() {
-    return ExpansionPanelList(
-        expansionCallback: (panelIndex, isExpanded) {
-          setState(() {
-            widget.peers[panelIndex].expanded = !isExpanded;
-          });
-        },
-        children: widget.peers.map((peer) {
-          return ExpansionPanel(
-              headerBuilder: (context, isExpanded) {
-                return ListTile(
-                  leading: const Icon(Icons.devices),
-                  title: Row(children: [
-                    const Text(
-                      "peer id: ",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Flexible(child: SelectableText(peer.peerId))
-                  ]),
-                );
-              },
-              isExpanded: peer.expanded,
-              body: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: peer.addrs.map((addr) {
-                  return ListTile(
-                    leading: const Icon(Icons.account_tree_rounded),
-                    title: SelectableText(addr),
-                  );
-                }).toList(),
-              ));
-        }).toList());
+  Widget _buildPeerList(List<PeerInfo> peers) {
+    return ListView.builder(
+      itemCount: peers.length,
+      itemBuilder: (context, index) {
+        final peer = peers[index];
+
+        List<Widget> children = [];
+        if (peer.addrs.isNotEmpty) {
+          children = [
+            const Divider(
+              thickness: 1.0,
+              height: 1.0,
+            ),
+          ];
+          children.addAll(peer.addrs.map((addr) {
+            return ListTile(
+              leading: const Icon(Icons.account_tree_rounded),
+              title: SelectableText(addr),
+            );
+          }));
+        }
+
+        return Card(
+          child: ExpansionTile(
+            leading: const Icon(Icons.devices),
+            title: SelectableText(peer.peerId),
+            children: children,
+          ),
+        );
+      },
+    );
   }
 }
